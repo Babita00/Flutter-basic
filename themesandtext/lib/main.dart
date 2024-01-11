3a85b6e7698a4b4970d2291d10ff77385a67e46a
Babita00 <babitaasth07@gmail.com>
2024-01-11 11:38:08 +0545

    2nd commit

--- themesandtext/lib/main.dart
+++ themesandtext/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:themesandtext/home_page.dart';
+import 'package:themesandtext/Pages/home_page.dart';
 
 void main() {
   runApp(const MyApp()); //runApp euta method ho
